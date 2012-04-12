0cb9d6a53199f03dda999f420f2579d416db3c93
Joel Byrnes <joel@newt.local>
2012-04-12 17:56:44 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RealestateRails::Application.config.secret_token = 'c4b5bf0ffd78c485e7a74ce25d0d6cb3c5c1725b23ca4490b3001b54904e236d517f81397c26f61d0acc5f8259893354cb3e58a2bd48746f4976729f48d819d4'
